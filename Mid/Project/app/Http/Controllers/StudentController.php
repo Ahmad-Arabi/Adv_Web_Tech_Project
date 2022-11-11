5ce57ffe0223832beade108f3b753edcdd48af0a
Ahmad-Arabi <65108308+Ahmad-Arabi@users.noreply.github.com>
2022-11-11 20:36:05 +0600

    Mid project uploaded

---
+++ Mid/Project/app/Http/Controllers/StudentController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\mail;
+use App\Models\User;
+use App\Mail\mailHelperClass;
+
+
+class StudentController extends Controller
+{
+    function ll()
+    {
+        //
+    }
+}
